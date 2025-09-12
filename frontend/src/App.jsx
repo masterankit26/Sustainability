40463f75068b34054b0cc659a4e23d6a6b5d306d
Ankit Shaw <153160423+masterankit26@users.noreply.github.com>
2025-09-12 12:21:35 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,47 +1,51 @@
-import React, { useState } from "react";
-import Navbar from "./components/Navbar";
-import MapPicker from "./components/MapPicker";
-import InsightPanel from "./components/InsightPanel";
-import SimulationTool from "./components/SimulationTool";
-import SensorChart from "./components/SensorChart";
-import EnergyDashboard from "./components/EnergyDashboard";
-import WeatherTimeline from "./components/WeatherTimeline";
-import IrrigationAdvisor from "./components/IrrigationAdvisor";
-import SetupGuide from "./components/SetupGuide";
-import SmartIrrigationOverview from "./components/SmartIrrigationOverview";
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
+import MapPicker from './components/MapPicker';
+import SimulationTool from './components/SimulationTool';
+import InsightPanel from './components/InsightPanel';
+import SensorChart from './components/SensorChart';
+import WeatherTimeline from './components/WeatherTimeline';
+import EnergyDashboard from './components/EnergyDashboard';
+import IrrigationAdvisor from './components/IrrigationAdvisor';
+// import ThankYou from './components/ThankYou';
+import SetupGuide from './components/SetupGuide';
+import SmartIrrigationOverview from './components/SmartIrrigationOverview';
 
 function App() {
   const [location, setLocation] = useState({ lat: null, lon: null });
 
-  const isValid = (val) => typeof val === "number" && !isNaN(val);
+  const isValid = (val) => typeof val === 'number' && !isNaN(val);
   const isLocationValid = isValid(location.lat) && isValid(location.lon);
 
   return (
     <div className="bg-gray-50 min-h-screen text-gray-900">
-      {/* Top Navigation */}
       <Navbar />
-
-      {/* Pick location */}
       <MapPicker onSubmit={setLocation} />
-
-      {/* Setup instructions */}
       <SetupGuide />
 
-      {/* Show dashboards only when location is valid */}
-      {isLocationValid && (
-        <>
+      {isLocationValid ? (
+        <div className="space-y-6 px-4 py-6">
           <IrrigationAdvisor
-            areaSize={500} // m²
+            areaSize={500}
             environment={location}
             location={location}
           />
           <InsightPanel location={location} />
+          
+          <EnergyDashboard location={location}/>
+         
           <SimulationTool location={location} />
-          <EnergyDashboard location={location} />
+           
           <SensorChart location={location} />
           <WeatherTimeline location={location} />
+         
           <SmartIrrigationOverview />
-        </>
+          {/* <ThankYou /> */}
+        </div>
+      ) : (
+        <div className="text-center py-10 text-red-600 font-semibold">
+          📍 Please select a valid location to activate smart features.
+        </div>
       )}
     </div>
   );
