d6c841990a50d2c55b95b2a7e0d7925af3d608d2
Ankit Shaw <153160423+masterankit26@users.noreply.github.com>
2025-09-12 13:39:23 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import MapPicker from './components/MapPicker';
 import SimulationTool from './components/SimulationTool';
 import InsightPanel from './components/InsightPanel';
 import SensorChart from './components/SensorChart';
+import EnergyDashboard from './components/EnergyDashboard';
 import WeatherTimeline from './components/WeatherTimeline';
 import IrrigationAdvisor from './components/IrrigationAdvisor';
 import SetupGuide from './components/SetupGuide';
@@ -29,6 +30,7 @@ function App() {
             location={location}
           />
           <InsightPanel location={location} />
+          <EnergyDashboard location={location}/>
           
           
          
